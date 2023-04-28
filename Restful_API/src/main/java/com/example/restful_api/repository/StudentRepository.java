5940d20822669940e80e493097089c498519e6fa
tuanna <1901040189@s.hanu.edu.vn>
2023-04-28 17:38:33 +0700

    new add

--- Restful_API/src/main/java/com/example/restful_api/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.restful_api.repository;
-
-import com.example.restful_api.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
--- SpringBoot2/src/main/java/com/example/springboot2/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springboot2.repository;
-
-import com.example.springboot2.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
